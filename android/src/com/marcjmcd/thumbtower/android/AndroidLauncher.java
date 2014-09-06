fd456f461ef1eeb644e9f56e89d3dc64d93651b5
marcjmcdougall <marcjmcdougall@gmail.com>
2014-09-06 16:54:06 -0400

    Initial commit.

---
+++ android/src/com/marcjmcd/thumbtower/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.marcjmcd.thumbtower.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.marcjmcd.thumbtower.ThumbTower;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ThumbTower(), config);
+	}
+}
