98ab13f7bfc2f9c47297739c284b180b51222f8e
marcjmcdougall <marcjmcdougall@gmail.com>
2014-09-12 20:22:51 -0400

    Added basic initialization methods for the GameMap class.  NOTE: must reverse column and row logic to support horizontal tile movement.  Also, consider fluid movement.

--- core/src/com/marcjmcd/thumbtower/models/GameMap.java
+++ core/src/com/marcjmcd/thumbtower/models/GameMap.java
@@ -1,35 +1,78 @@
 package com.marcjmcd.thumbtower.models;
 
+import java.util.Iterator;
+
+import sun.rmi.runtime.NewThreadAction;
+
 import com.badlogic.gdx.utils.Array;
 import com.marcjmcd.thumbtower.models.tiles.BasicTile;
 
 public class GameMap {
 
+	public static final int MAX_ROWS = 10;
+	public static final int MAX_COLS = 15;
+	
 	private Array<Array<BasicTile>> primaryTiles;
 	private Array<BasicTile> tileColumnBuffer;
 	
+	// Iterator that moves through the rows of Arrays of basic tiles.  Moves vertically.
+	private Iterator<Array<BasicTile>> tileRowIterator;	
+	
+	// Iterator that will iterate through each row of basic tiles.  Moves horizontally.
+	private Iterator<BasicTile> tileColumnIterator;
+	
+	// Iterator that will iterate through the buffer overflow array.  Moves vertically.
+	private Iterator<BasicTile> tileColumnBufferIterator;
+	
 	public GameMap(){
 		
-		this.primaryTiles = new Array<Array<BasicTile>>();
 		this.tileColumnBuffer = new Array<BasicTile>();
 		
-		populatePrimaryTiles();
-		populateSecondaryTiles();
+		initializePrimaryTiles();
+		initializeBufferTiles();
+		
+		this.tileColumnBufferIterator = tileColumnBuffer.iterator();
 	}
 
-	private void populateSecondaryTiles() {
+	private void initializePrimaryTiles() {
 		
-		// TODO: Implementation.
-	}
-
-	private void populatePrimaryTiles() {
+		this.primaryTiles = new Array<Array<BasicTile>>(MAX_ROWS);
 		
-		// TODO: Implementation.
+		for(int i = 0; i < MAX_ROWS; i++){
+			
+			Array<BasicTile> newTiles = new Array<BasicTile>(MAX_COLS);
+			
+			for(int j = 0; j < MAX_COLS; j++){
+				
+				newTiles.add(new BasicTile());
+			}
+			
+			primaryTiles.add(newTiles);
+		}
+		
+		// Once the data structure is completely initialized, extract the iterator.
+		this.tileRowIterator = primaryTiles.iterator();
+		
+		// Initialized to null now - it will be reinitialized with each new row extracted from the main array.
+		this.tileColumnIterator = null;
 	}
 	
-	private void generateColumn(){
+	private void initializeBufferTiles() {
 		
-		// TODO: Implementation.
+		this.tileColumnBuffer = new Array<BasicTile>(MAX_ROWS);
+		
+		generateColumn();
+	}
+
+	private void generateColumn(){
+
+		for(int i = 0; i < MAX_ROWS; i++){
+			
+			tileColumnBuffer.add(new BasicTile());
+		}
+		
+		// Once the data structure is completely initialized, extract the iterator.
+		this.tileColumnBufferIterator = tileColumnBuffer.iterator();
 	}
 	
 	public void shiftTiles(){
